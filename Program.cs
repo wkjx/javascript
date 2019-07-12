8af328a0567af3c2d496ba5ee1ea78c820bcae95
hhxw <278994463@qq.com>
2019-07-12 21:15:58 +0800

    sdf

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            String a = new string("as");
+            a.ToUpper();
+            Console.Write(a.ToUpper());
+            Console.ReadKey(true);
+            String B = @"c:\Windows\System32";
+
+
+        }
+    }
+}
